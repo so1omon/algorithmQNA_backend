536ca7c46d06bceb17a82589ac879e9d270bb623
janguni <lightson23@naver.com>
2023-04-05 22:40:06 +0900

    docs: add REAME.md

---
+++ src/main/java/algorithm_QnA_community/algorithm_QnA_community/AlgorithmQnACommunityApplication.java
@@ -0,0 +1,13 @@
+package algorithm_QnA_community.algorithm_QnA_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmQnACommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmQnACommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/algorithm_QnA_community/algorithm_QnA_community/AlgorithmQnACommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package algorithm_QnA_community.algorithm_QnA_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmQnACommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
