01772ae5a7791cdf58311c7159535d7416ca5645
so1omon <solmin3665@gmail.com>
2023-05-19 18:18:49 +0900

    modify: memberRole field added into MemberBriefDtor

--- src/main/java/algorithm_QnA_community/algorithm_QnA_community/api/controller/MemberBriefDto.java
+++ src/main/java/algorithm_QnA_community/algorithm_QnA_community/api/controller/MemberBriefDto.java
@@ -1,6 +1,7 @@
 package algorithm_QnA_community.algorithm_QnA_community.api.controller;
 
 import algorithm_QnA_community.algorithm_QnA_community.domain.member.Member;
+import algorithm_QnA_community.algorithm_QnA_community.domain.member.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,14 +24,17 @@ import lombok.NoArgsConstructor;
 public class MemberBriefDto {
     private Long memberId;
     private String memberName;
+    private Role memberRole;
     private int memberCommentBadge;
     private int memberPostBadge;
     private int memberLikeBadge;
     private String memberProfileUrl;
 
+
     public MemberBriefDto(Member member){
         this.memberId = member.getId();
         this.memberName = member.getName();
+        this.memberRole = member.getRole();
         this.memberCommentBadge = member.getCommentBadgeCnt();
         this.memberPostBadge =member.getPostBadgeCnt();
         this.memberLikeBadge = member.getLikeBadgeCnt();
