2680e3e3162dc8ae3d3b77ad6068227d831653d4
so1omon <solmin3665@gmail.com>
2023-06-26 14:59:59 +0900

    fix: timezone set in Main class @PostConstruct

--- src/main/java/algorithm_QnA_community/algorithm_QnA_community/AlgorithmQnACommunityApplication.java
+++ src/main/java/algorithm_QnA_community/algorithm_QnA_community/AlgorithmQnACommunityApplication.java
@@ -6,12 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class AlgorithmQnACommunityApplication {
+
+	@PostConstruct
+	void started() {
+		// timezone 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgorithmQnACommunityApplication.class, args);
